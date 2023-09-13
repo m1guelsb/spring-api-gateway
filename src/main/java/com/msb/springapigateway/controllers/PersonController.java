822ae590d681bb08de4da883fa866f7b039b7c68
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-09-13 09:40:37 -0300

    [feat] add DozerMapper

--- src/main/java/com/msb/springapigateway/controllers/PersonController.java
+++ src/main/java/com/msb/springapigateway/controllers/PersonController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.msb.springapigateway.models.Person;
+import com.msb.springapigateway.data.vo.v1.PersonVO;
 import com.msb.springapigateway.services.PersonService;
 
 @RestController
@@ -24,23 +24,23 @@ public class PersonController {
   private PersonService service;
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Person> findById() {
+  public List<PersonVO> findById() {
     return service.findAll();
   }
 
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Person findById(@PathVariable(value = "id") Long id) {
+  public PersonVO findById(@PathVariable(value = "id") Long id) {
     return service.findById(id);
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Person create(@RequestBody Person person) {
-    return service.create(person);
+  public PersonVO create(@RequestBody PersonVO personVO) {
+    return service.create(personVO);
   }
 
   @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Person update(@RequestBody Person person) {
-    return service.update(person);
+  public PersonVO update(@RequestBody PersonVO personVO) {
+    return service.update(personVO);
   }
 
   @DeleteMapping(value = "/{id}")
--- src/main/java/com/msb/springapigateway/services/PersonService.java
+++ src/main/java/com/msb/springapigateway/services/PersonService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.msb.springapigateway.data.vo.v1.PersonVO;
 import com.msb.springapigateway.exceptions.ResourceNotFoundException;
+import com.msb.springapigateway.mapper.DozerMapper;
 import com.msb.springapigateway.models.Person;
 import com.msb.springapigateway.repositories.PersonRepository;
 
@@ -18,36 +20,45 @@ public class PersonService {
 
   private Logger logger = Logger.getLogger(PersonService.class.getName());
 
-  public List<Person> findAll() {
+  public List<PersonVO> findAll() {
     logger.info("Finding all Person");
 
-    return repository.findAll();
+    return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
   }
 
-  public Person findById(Long id) {
+  public PersonVO findById(Long id) {
     logger.info("Finding one Person");
 
-    return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
-  }
-
-  public Person create(Person person) {
-    logger.info("Creating one Person");
-
-    return repository.save(person);
-  }
-
-  public Person update(Person person) {
-    logger.info("Updating one Person");
-
-    Person entity = repository.findById(person.getId())
+    var entity = repository.findById(id)
         .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
 
-    entity.setFirstName(person.getFirstName());
-    entity.setLastName(person.getLastName());
-    entity.setAddress(person.getAddress());
-    entity.setGender(person.getGender());
+    return DozerMapper.parseObject(entity, PersonVO.class);
+  }
 
-    return repository.save(person);
+  public PersonVO create(PersonVO personVO) {
+    logger.info("Creating one Person");
+
+    var entity = DozerMapper.parseObject(personVO, Person.class);
+
+    var savedPersonVO = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+
+    return savedPersonVO;
+  }
+
+  public PersonVO update(PersonVO personVO) {
+    logger.info("Updating one Person");
+
+    Person entity = repository.findById(personVO.getId())
+        .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+    entity.setFirstName(personVO.getFirstName());
+    entity.setLastName(personVO.getLastName());
+    entity.setAddress(personVO.getAddress());
+    entity.setGender(personVO.getGender());
+
+    var savedPersonVO = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+
+    return savedPersonVO;
   }
 
   public ResponseEntity<?> delete(Long id) {
